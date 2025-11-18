e1b407cef0c81475a47de33c5f2bbbef9d82c4e5
surwxa <nepalisurakshya767@gmail.com>
2025-11-18 22:01:32 +0545

    adding some comment

--- To-do list/script.js
+++ To-do list/script.js
@@ -11,17 +11,21 @@ class toDoApp{
     const taskText=this.TaskInput.value
     if(!taskText) return;
     this.tasks.push(taskText)
+    // render task used to show the task
     this.renderTask()
+     // After successfully adding a task, this line clears the input box so the user can type the next task without deleting manually.
     this.TaskInput.value=""
     
   }
   renderTask(){
+    // .innerhtml clears all existing items before adding new ons
     this.Tasklist.innerHTML="";
     this.tasks.forEach((task,index)=>{
       const li =document.createElement("li")
       li.textContent=task
+       // Adds (attaches) the <li> to the <ul> or <ol> in the webpage.
+      // So tasks appear visually on the screen.
       this.Tasklist.appendChild(li)
-
     })
 
 
