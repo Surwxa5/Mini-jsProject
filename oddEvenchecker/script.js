b748043df9b3bd92c1fcf3fec4a2dd4a7a49717c
surwxa <nepalisurakshya767@gmail.com>
2025-11-24 11:23:31 +0545

    correcting code

--- oddEvenchecker/script.js
+++ oddEvenchecker/script.js
@@ -2,7 +2,6 @@ const num = document.getElementById("typeNum");
 
 function prints() {
   let a = Number(num.value);   // FIXED
-  console.log(a);
 
   if (a % 2 === 0) {
     document.getElementById("result1").innerText = "Even";
