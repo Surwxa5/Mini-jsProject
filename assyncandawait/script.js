9d8b42c15f2b009bd4bf507c572f737a21210c54
surwxa <nepalisurakshya767@gmail.com>
2025-11-28 20:56:44 +0545

    deleting the file

---
+++ assyncandawait/script.js
@@ -0,0 +1,25 @@
+// async function always returns a Promise. Other values returned by an async function are automatically wrapped in a resolved Promise.
+async function assync(){
+  document.getElementById("result").textContent="wellcome to js"
+}
+
+// show the result after 2000ms means 2 sec
+// setInterval(() => {
+//   alert("hello")
+// }, 2000);
+
+// setInterval(()=>{
+//   alert('hello')
+// }
+// ,5000
+// )
+
+// it will only show one time
+// setTimeout(()=>{
+//   alert('hello')
+// },2000)
+
+
+async function viewData(){
+  
+}
