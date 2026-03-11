831a99d231b964ff97936be03f691eaf45bff027
Surwxa5 <nepalisurakshya767@gmail.com>
2026-03-11 22:14:46 +0545

    smart to to list

---
+++ To-do list/smart-To-Do-List.js
@@ -0,0 +1,8 @@
+const userInput = document.getElementById('user-input-task')
+const Task = document.getElementById('task')
+
+
+const displayTask = () =>{
+const userInputTask= Number(userInput.value)
+
+}
