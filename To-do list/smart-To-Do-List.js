d3801e38cefb2dd631ca11074b8860134c4658ff
Surwxa5 <nepalisurakshya767@gmail.com>
2026-03-13 14:38:25 +0545

    to do list

--- To-do list/smart-To-Do-List.js
+++ To-do list/smart-To-Do-List.js
@@ -1,8 +1,42 @@
+taskList = []
+
+
 const userInput = document.getElementById('user-input-task')
 const Task = document.getElementById('task')
 
 
 const displayTask = () =>{
-const userInputTask= Number(userInput.value)
-
+const userInputTask= userInput.value
+if(userInputTask === ''){
+  alert("Type something first")
+  return
 }
+else{
+ const taskObject ={
+  name:userInputTask,
+  priority:"high",
+  id : Date.now()
+ }
+
+  const newText = document.createElement("li")
+  
+  newText.textContent=taskObject.name + ''
+
+  const removeButton = document.createElement("button")
+
+  removeButton.addEventListener("click",() =>{
+    newText.remove()
+  })
+
+  removeButton.style.marginLeft ="10px"
+  removeButton.style.color = 'red'
+  removeButton.textContent = "delete"
+  newText.appendChild(removeButton)
+  Task.appendChild(newText)
+
+ }
+}
+document.getElementById("addTask-btn").addEventListener("click",displayTask)
+
+
+
