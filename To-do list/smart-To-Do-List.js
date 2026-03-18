42bc6e5a25e674d38865970cfe7c33d53bd1dc4c
Surwxa5 <nepalisurakshya767@gmail.com>
2026-03-18 21:45:20 +0545

    complete one step below

--- To-do list/smart-To-Do-List.js
+++ To-do list/smart-To-Do-List.js
@@ -1,60 +1,69 @@
 let taskList = []; // Your storage box
 
 const addTask = () => {
-const userInputTask= userInput.value
-if(userInputTask === ''){
-  alert("Type something first")
-  return
-}
- const taskObject ={
-  name:userInputTask,
-  priority:"high",
-  id : Date.now()
- }
-  taskList.push(taskObject); 
-renderTasks()
-userInput.value = ""
-  
+  const userInput = document.getElementById('user-input-task')
+  const userInputTask = userInput.value
+  if (userInputTask === '') {
+    alert("Type something first")
+    return
+  }
+  const taskObject = {
+    name: userInputTask,
+    priority: "high",
+    id: Date.now()
+  }
+  taskList.push(taskObject);
+  renderTasks()
+  userInput.value = ""
+
 }
 
-const userInput = document.getElementById('user-input-task')
-const Task = document.getElementById('task')
+
 
 
 const renderTasks = () => {
-Task.innerHTML = ''
-taskList.forEach((task)=>{
-  const newText = document.createElement("li")
-  
-  newText.textContent=taskObject.name + ''
 
-  const removeButton = document.createElement("button")
+  const count = document.getElementById("countDisplay")
+  const Task = document.getElementById('task')
 
-  removeButton.addEventListener("click",() =>{
-    newText.remove()
+  Task.innerHTML = ''
+  taskList.forEach((taskObject) => {
+    const newText = document.createElement("li")
+
+    newText.textContent = taskObject.name + ''
+
+    const removeButton = document.createElement("button")
+
+    removeButton.addEventListener("click", () => {
+      deleteTask(task.id)
+    })
+
+    removeButton.style.marginLeft = "10px"
+    removeButton.style.color = 'red'
+    removeButton.textContent = "delete"
+    newText.appendChild(removeButton)
+    Task.appendChild(newText)
+
+     removeButton.className = "delete-btn"
+
+
+
   })
-
-  removeButton.style.marginLeft ="10px"
-  removeButton.style.color = 'red'
-  removeButton.textContent = "delete"
-  newText.appendChild(removeButton)
-  Task.appendChild(newText)
-
-  // will do it tomorrow 
-
-
-
-})
-
+  count.textContent = `Total Tasks: ${taskList.length}`;
 }
 const deleteTask = (id) => {
   // Filter out the task with the matching ID
-  taskListArray = taskListArray.filter(task => task.id !== id);
-  
+   taskList = taskList.filter(task => task.id !== id);
+
   // Re-draw the list
   renderTasks();
 };
-document.getElementById("addTask-btn").addEventListener("click",renderTasks)
+document.getElementById("addTask-btn").addEventListener("click", addTask)
+   document.getElementById("user-input-task").addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            addTask()
+        }
+    })
 
 
 
